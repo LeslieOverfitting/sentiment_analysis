12ce3ed16132966d037e795bf9f963bd78f1c518
leslie <296526299@qq.com>
2020-04-03 18:04:39 +0800

    clean

--- utils.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import timedelta
-
-def get_time_diff(start_time):
-    """获取已使用时间"""
-    end_time = time.time()
-    time_dif = end_time - start_time
-    return timedelta(seconds=int(round(time_dif)))
